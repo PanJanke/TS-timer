452b5a092bf5ff1e31bdf33063a62641bb5b3557
Jan Krol <jan.krol.legowski@gmail.com>
2024-06-14 02:46:24 +0200

    reducer

--- src/components/store/timers-context.tsx
+++ src/components/store/timers-context.tsx
@@ -1,4 +1,4 @@
-import {createContext, ReactNode, useContext} from "react";
+import {createContext, ReactNode, useContext, useReducer, useState} from "react";
 
 type Timer = {
     name: string;
@@ -9,6 +9,11 @@ type TimersState = {
     isRunning: boolean;
     timers: Timer[]
 };
+
+const initialState: TimersState = {
+    isRunning: true,
+    timers: []
+}
 
 type TimersContextValue = TimersState & {
     addTimer: (timerData: Timer) => void,
@@ -21,7 +26,7 @@ export const TimersContext = createContext<TimersContextValue | null>(null);
 export function useTimersContext() {
     const timersCtx = useContext(TimersContext);
 
-    if (timersCtx === null){
+    if (timersCtx === null) {
         throw new Error('TimersContext is null - that should not be the case !');
     }
 
@@ -32,19 +37,54 @@ type TimersContextProviderPros = {
     children: ReactNode;
 };
 
-export function TimersContextProvider({children}: TimersContextProviderPros) {
+
+type Action = {
+    type: 'ADD_TIMER' | 'START_TIMERS' | 'STOP_TIMERS';
+};
+function timersReducer(state: TimersState, action: Action): TimersState {
+    if(action.type === 'START_TIMERS'){
+        return {
+            ...state,
+            isRunning: true
+        }
+    }
+
+    if(action.type === 'STOP_TIMERS'){
+        return {
+            ...state,
+            isRunning: false
+        }
+    }
+
+    if(action.type === 'ADD_TIMER'){
+        return {
+            ...state,
+            timers: [
+                ...state.timers,
+                {
+                    name,
+                    duration
+                }
+            ]
+        }
+    }
+}
+
+export default function TimersContextProvider({children}: TimersContextProviderPros) {
+
+    const [timersState, dispatch] = useReducer(timersReducer, initialState)
 
     const ctx: TimersContextValue = {
         timers: [],
         isRunning: false,
         addTimer(timerData) {
-            ///,,,,
+           dispatch({type: 'ADD_TIMER'});
         },
         startTimers() {
-            ///...
+            dispatch({type: 'START_TIMERS'});
         },
         stopTimers() {
-            ///,,,
+            dispatch({type: 'STOP_TIMERS'});
         }
     }
 
