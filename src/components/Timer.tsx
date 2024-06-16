061430cf3c03afbcefc0aa34132697fa922b0f55
Pan_Janke <jan.krol.legowski@gmail.com>
2024-06-16 04:15:23 +0200

    small fix

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,27 +1,34 @@
 import Container from './UI/Container.tsx';
-import {type Timer as TimerProps} from "./store/timers-context.tsx";
+import {type Timer as TimerProps, useTimersContext} from "./store/timers-context.tsx";
 import {useEffect, useRef, useState} from "react";
 
 
 export default function Timer({name, duration}: TimerProps) {
     const interval = useRef<number | null>(null);
     const [remainingTime, setRemainingTime] = useState(duration * 1000);
+    const {isRunning} = useTimersContext();
 
-    if(remainingTime<=0 && interval.current){
+    if (remainingTime <= 0 && interval.current) {
         clearInterval(interval.current);
     }
 
     useEffect(() => {
-        const timer = setInterval(function () {
-            setRemainingTime((prevTime) => prevTime - 50);
-        }, 50);
+        let timer: number;
 
-        interval.current = timer;
+        if (isRunning) {
+            timer = setInterval(function () {
+                setRemainingTime((prevTime) => prevTime - 50);
+            }, 50);
+            interval.current=timer;
+
+        } else if (interval.current) {
+            clearInterval(interval.current);
+        }
 
         return () => clearInterval(timer);
-    }, []);
+    }, [isRunning]);
 
-    const formattedRemainingTime = (remainingTime/1000).toFixed(2);
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
     return (
         <Container as="article">
